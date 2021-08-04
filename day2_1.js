f9e8fc09913e759267b96ea09723164579b2d98a
Saikrishhhhh <88379742+Saikrishhhhh@users.noreply.github.com>
2021-08-04 16:25:09 +0530

    Day 2 assignment submitted

---
+++ day2_1.js
@@ -0,0 +1,5 @@
+let arr=["Ram","Sai","Kumar","Krishna","Suresh"];
+for(i=4;i>=0;i--)
+{
+console.log(arr[i]);
+}
---
+++ day2_2.js
@@ -0,0 +1,7 @@
+for(i=1;i<51;i++)
+{
+    if(i%5==0)
+    {
+        console.log(i);
+    }
+}
